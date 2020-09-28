960beb686b0d51b2faf0fbf5050378a5353d00db
andrei <andrei.trusenko@gmail.com>
2020-09-28 16:36:41 +0300

    added styles for main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import { PhotosList } from './features/photos/PhotosList'
 import { SinglePhotoPage } from './features/photos/SinglePhotoPage'
 import { Auth } from './api/Auth.js'
+import { Header } from './features/photos/Header'
+
+import './features/photos/styles/font-faces.css'
+import './features/photos/styles/normalize.css'
+import './features/photos/styles/loading-animation.css'
+import './features/photos/styles/mansory-grid.css'
+import './features/photos/styles/header.css'
+import './features/photos/styles/photo-list.css'
+import './features/photos/styles/single-photo-page.css'
 
 function App() {
   return (
@@ -14,6 +23,7 @@ function App() {
           path="/"
           render={() => (
             <React.Fragment>
+              <Header />
               <PhotosList />
             </React.Fragment>
           )}
---
+++ src/features/photos/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className="header">
+        <div className="logo">
+          <p>INSTASPLASH</p>
+        </div>
+      </header>
+  )
+}
