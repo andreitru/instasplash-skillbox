d76bbc7b063f9adc77b754cb3894e5938618f54b
andrei <andrei.trusenko@gmail.com>
2020-09-28 16:36:41 +0300

    added styles for main page

--- src/features/photos/PhotosList.js
+++ src/features/photos/PhotosList.js
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { selectAllPhotos, fetchPhotos } from './photosSlice'
 import Cookies from 'js-cookie'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Masonry from 'react-masonry-css'
+import { formatRelative, subDays } from 'date-fns'
 
 export const PhotosList = () => {
   const dispatch = useDispatch()
@@ -17,30 +20,50 @@ export const PhotosList = () => {
     }
   }, [photosStatus, dispatch, page])
 
-  let content
-
-  if (photosStatus === 'loading') {
-    content = <div>Loading...</div>
-  } else if (photosStatus === 'succeeded') {
-    content = photos.map(photo => (
-    <article key={photo.id}>
-      <Link to={`/photos/${photo.id}`}>
-        <img src={photo.urls.small} alt={photo.alt_description}  />
-      </Link>
-      <p>{photo.likes}</p>
-      <a href={photo.user.links.html} target='_blank' rel='noreferrer noopener'><img src={photo.user.profile_image.medium} alt={photo.user.name}/>{photo.user.name}</a>
-      <p>{photo.created_at}</p>
-    </article>
-    ))
-  } else if (photosStatus === 'failed') {
-    content = <div>{error}</div>
+  const breakpointColumnsObj = {
+    default: 4,
+    1024: 3,
+    768: 2,
+    480: 1
   }
 
   return (
+    
     <section>
-      <h2>Photos</h2>
-      {content}
-      <button onClick={() => dispatch(fetchPhotos(page))}>Загрузить ещё</button>
+      <div style={(photosStatus === 'failed') ? {display: "block"} : {display: "none"}} className="error">Error: {error}</div>
+      <div style={(photosStatus === 'failed') ? {display: "none"} : {display: "block"}} className="container">
+      <InfiniteScroll
+        dataLength={photos.length}  
+        next={() => dispatch(fetchPhotos(page))}
+        hasMore={true}
+        loader={<div className="lds-ripple"><div></div><div></div></div>}
+      >
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+      
+        {photos.map(photo => (
+          <article key={photo.id} className="photo">
+            <div className="top">
+              <a href={photo.user.links.html} target='_blank' rel='noreferrer noopener' className="profile-link">
+                <img src={photo.user.profile_image.medium} alt={photo.user.name} className="profile-img"/>
+                {photo.user.name}
+              </a>
+            </div>
+            <Link to={`/photos/${photo.id}`}>
+              <img src={photo.urls.small} alt={photo.alt_description}/>
+            </Link>
+            <div className="bottom">
+              <p className="likes">{photo.likes} likes</p>
+              <p className="date">{formatRelative(subDays(new Date(photo.created_at), 0), new Date())}</p>
+            </div>
+          </article>))}
+          
+          </Masonry>
+      </InfiniteScroll> 
+      </div>
     </section>
   )
 }
--- src/features/photos/SinglePhotoPage.js
+++ src/features/photos/SinglePhotoPage.js
@@ -8,7 +8,8 @@ export const SinglePhotoPage = ({ match }) => {
   const { photoId } = match.params
 
   const photo = useSelector(state => selectPhotoById(state, photoId))
-  const status = useSelector(state => state.photos.likeStatus)
+  const likeStatus = useSelector(state => state.photos.likeStatus)
+  const singlePhotoStatus = useSelector(state => state.photos.fetchPhotoStatus)
   const error = useSelector(state => state.photos.likeError)
   const likes = photo.likes
   const id = photo.id
@@ -24,21 +25,22 @@ export const SinglePhotoPage = ({ match }) => {
 
   let isLiked;
   let likeCounter;
-  if (status === 'loading') {
+  if (likeStatus === 'loading') {
     isLiked = <p>Loading...</p>
     likeCounter = <p>Loading...</p>
-  } else if (status === 'succeeded') {
+  } else if (likeStatus === 'succeeded' || singlePhotoStatus === 'succeeded') {
     isLiked = <p>{photo.liked_by_user ? 'liked' : 'not liked'}</p>
     likeCounter = <p>{likes}</p>
     dispatch(likedPhoto({id, likes}))
-  } else if (status === 'failed') {
+  } else if (likeStatus === 'failed') {
     isLiked = <p>{error}</p>
     likeCounter = <p>{error}</p>
   }
 
   return (
     <section>
-       <Link to="/">Назад</Link>
+       {/* <Link to="/">Назад</Link> */}
+       <button onClick={() => window.history.back()}>Назад</button>
       <article>
         <img src={photo.urls.regular} alt={photo.alt_description} 
           onLoad={() => dispatch(fetchSinglePhoto(id))
@@ -54,6 +56,7 @@ export const SinglePhotoPage = ({ match }) => {
         </button>
         {isLiked}
       </article>
+     
     </section>
   )
 }
